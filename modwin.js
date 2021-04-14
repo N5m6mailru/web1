35bd8f6bbb4a5707e5b4d96a0d559335891ba593
Irina <n5m6@mail.ru>
2021-04-14 10:29:09 +0300

    mod_win

---
+++ modwin.js
@@ -0,0 +1,28 @@
+    "use strict";
+
+    $(document).ready(function($) {
+        $('.popup-open').click(function() {
+            $('.popup-fade').fadeIn();
+            return false;
+        });	
+        // Клик по ссылке "Закрыть".
+        $('.popup-close').click(function() {
+            $(this).parents('.popup-fade').fadeOut();
+            return false;
+        });        
+     
+        // Закрытие по клавише Esc.
+        $(document).keydown(function(e) {
+            if (e.keyCode === 27) {
+                e.stopPropagation();
+                $('.popup-fade').fadeOut();
+            }
+        });
+        
+        // Клик по фону, но не по окну.
+        $('.popup-fade').click(function(e) {
+            if ($(e.target).closest('.popup').length == 0) {
+                $(this).fadeOut();					
+            }
+        });	
+    });
